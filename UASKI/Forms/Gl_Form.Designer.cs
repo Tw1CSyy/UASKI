90d873f1a711eb0fc98c4dba8cb4f73d6b4fdbed
Ir1s <ya.breyner@gmail.com>
2025-04-08 15:01:49 +0300

    UASKI 2.4

--- UASKI/Forms/Gl_Form.Designer.cs
+++ UASKI/Forms/Gl_Form.Designer.cs
@@ -3975,7 +3975,7 @@ namespace UASKI
             this.KeyPreview = true;
             this.Name = "Gl_Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = " УАСКИ 2.36";
+            this.Text = " УАСКИ 2.4";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Gl_Form_KeyDown);
             this.Resize += new System.EventHandler(this.Gl_Form_Resize);
             this.tabControl1.ResumeLayout(false);
